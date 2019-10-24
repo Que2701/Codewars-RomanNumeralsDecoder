1bfa96af2d672ca0746b9699db9f5b8688e48088
tumelo <tumelom@evolvemed.co.za>
2019-10-24 15:34:40 +0200

    added a git ignore file

---
+++ RomanNumeralsDecoder/RomanNumeralsDecoderApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumeralsDecoderApp
+{
+    /// <summary>
+    /// Main program
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Main method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
